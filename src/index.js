2925a9ae421f65d57042fdcbca95857db53648ea
Alison234 <Garkovenko_setevoe@mail.ru>
2020-09-17 23:04:18 +0300

    basic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const canvas = document.getElementById("cnvs");
-
+const score = document.getElementById('score');
+const restart = document.getElementById('restart')
 const gameState = {};
 
 function onMouseMove(e) {
     gameState.pointer.x = e.pageX;
-    gameState.pointer.y = e.pageY
+    gameState.pointer.y = e.pageY;
+}
+
+restart.addEventListener('click',startGame,false);
+
+function windowLoser(context) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.textAlign = "center";
+    context.fillStyle = "#ff0000";
+    context.font = "italic 50pt Impact";
+    context.fillText("Game Over! Your total score is: " + gameState.TotalScore,canvas.width / 2, canvas.height / 2, canvas.width/2);
+    context.closePath();
+    restart.style.visibility = 'visible';
 }
 
 function queueUpdates(numTicks) {
@@ -16,30 +30,78 @@ function queueUpdates(numTicks) {
 
 function draw(tFrame) {
     const context = canvas.getContext('2d');
-
     // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
-
-    drawPlatform(context)
-    drawBall(context)
+    drawPlatform(context);
+    drawBall(context);
+    if(gameState.IsTimeToBonus && gameState.IsBonus){
+        drawBonus(context);
+    }
+    if(gameState.isLose){
+        windowLoser(context);
+    }
 }
 
 function update(tick) {
+    const vx = (gameState.pointer.x - gameState.player.x) / 10;
+    gameState.player.x += vx;
+    const ball = gameState.ball;
+    const bonus = gameState.bonus;
+    ball.y += ball.vy;
+    ball.x += ball.vx;
+    if(gameState.IsTimeToBonus && gameState.IsBonus) {
+        bonus.y2 += bonus.vy;
+        bonus.y1 += bonus.vy;
+        bonus.x1 += bonus.vx;
+        bonus.x2 += bonus.vx;
+    }
+    score.innerHTML = 'Score ' + gameState.TotalScore;
+    Conclusion();
+}
 
-    const vx = (gameState.pointer.x - gameState.player.x) / 10
-    gameState.player.x += vx
+function  Conclusion(){
 
-    const ball = gameState.ball
-    ball.y += ball.vy
-    ball.y += ball.vx
+    if(gameState.ball.y > gameState.player.y - gameState.platform.height && (gameState.ball.x  > gameState.player.x -  gameState.platform.width/2 &&
+        gameState.ball.x  < gameState.player.x + gameState.platform.width/2)){
+        gameState.ball.vy = -1*gameState.ball.vy;
+        console.log('Кординаты мяча: x' + gameState.ball.x + ' y ' + gameState.ball.y);
+        console.log('Кординаты платформф: x' + gameState.player.x + ' y ' + gameState.player.y);
+    }
+    if(gameState.ball.y > canvas.offsetHeight){
+        gameState.isLose = true;
+        stopGame(gameState.stopCycle);
+    }
+    if(gameState.ball.y < 0)
+        gameState.ball.vy =-1* gameState.ball.vy;
+
+    if(gameState.ball.x < 0)
+        gameState.ball.vx = -1*gameState.ball.vx;
+
+    if(gameState.ball.x > canvas.offsetWidth)
+        gameState.ball.vx = -1*gameState.ball.vx ;
+
+    if(gameState.IsBonus) {
+        if (gameState.bonus.y2 > canvas.offsetHeight)
+            gameState.IsBonus = false;
+
+        if (gameState.bonus.y2 + gameState.bonus.width > gameState.player.y - gameState.platform.height/2 && (gameState.bonus.x1> gameState.player.x - gameState.platform.width / 2 &&
+            gameState.bonus.x1 < gameState.player.x + gameState.platform.width / 2)) {
+            gameState.TotalScore += 15;
+            gameState.IsBonus = false;
+        }
+        if(gameState.bonus.x1 + gameState.bonus.width < 0)
+            gameState.bonus.vx = -1*gameState.bonus.vx;
+
+        if(gameState.bonus.x1  + gameState.bonus.width> canvas.offsetWidth)
+            gameState.bonus.vx = -1*gameState.bonus.vx ;
+
+    }
 }
 
 function run(tFrame) {
     gameState.stopCycle = window.requestAnimationFrame(run);
-
     const nextTick = gameState.lastTick + gameState.tickLength;
     let numTicks = 0;
-
     if (tFrame > nextTick) {
         const timeSinceTick = tFrame - gameState.lastTick;
         numTicks = Math.floor(timeSinceTick / gameState.tickLength);
@@ -57,40 +119,74 @@ function drawPlatform(context) {
     const {x, y, width, height} = gameState.player;
     context.beginPath();
     context.rect(x - width / 2, y - height / 2, width, height);
-    context.fillStyle = "#FF0000";
+    context.fillStyle = "#15c0cf";
+    context.shadowColor = "blue";
+    context.shadowBlur = 10;
     context.fill();
     context.closePath();
 }
+
+function drawBonus(context) {
+    const {x1,y1,x2,y2,width,height} = gameState.bonus;
+    context.beginPath();
+    context.rect(x1, y1, width, height);
+    context.rect(x2, y2, height, width);
+    context.fillStyle = "#f1657b";
+    context.fill();
+    context.closePath();
+}
+
 
 function drawBall(context) {
     const {x, y, radius} = gameState.ball;
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
-    context.fillStyle = "#0000FF";
+    context.fillStyle = "#bf34af";
     context.fill();
     context.closePath();
 }
 
+function  setupBonus(){
+    gameState.IsTimeToBonus = true;
+    gameState.IsBonus = true;
+    let randomBonusX = getRandomIntInclusive(0,canvas.width-100);
+    let randomBonusY = getRandomIntInclusive(0,100);
+    gameState.bonus = {
+        x1:randomBonusX,
+        x2:randomBonusX+10,
+        y1:randomBonusY,
+        y2:randomBonusY-10,
+        width:30,
+        height:10,
+        vy:getRandomIntInclusive(1,5),
+        vx:getRandomIntInclusive(1,5)
+    }
+    console.log('Setup to Bonus');
+}
+
 function setup() {
+    restart.style.visibility = 'hidden';
+    gameState.IsTimeToBonus = false;
+    gameState.IsBonus = false;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
     canvas.addEventListener('mousemove', onMouseMove, false);
-
     gameState.lastTick = performance.now();
     gameState.lastRender = gameState.lastTick;
     gameState.tickLength = 15; //ms
+    gameState.isLose = false;
+    gameState.TotalScore = 0;
 
-    const platform = {
+    let randomStartAngel = getRandomIntInclusive(-100,100)/10;
+    gameState.platform = {
         width: 400,
         height: 50,
     };
-
     gameState.player = {
         x: 100,
-        y: canvas.height - platform.height / 2,
-        width: platform.width,
-        height: platform.height
+        y: canvas.height - gameState.platform.height / 2,
+        width: gameState.platform.width,
+        height: gameState.platform.height
     };
     gameState.pointer = {
         x: 0,
@@ -98,12 +194,38 @@ function setup() {
     };
     gameState.ball = {
         x: canvas.width / 2,
-        y: 0,
+        y: 0 ,
         radius: 25,
-        vx: 0,
+        vx: randomStartAngel,
         vy: 5
     }
 }
 
-setup();
-run();
+function timer(){
+    setInterval(function (){
+        gameState.TotalScore ++;
+    },1000);
+    setInterval(function (){
+        if(!gameState.IsBonus) {
+            setupBonus();
+        }
+    },15000);
+    setInterval(function (){
+        gameState.ball.vx += gameState.ball.vx * 0.1;
+        gameState.ball.vy += gameState.ball.vy * 0.1;
+    },10000);
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.round(Math.random() * (max - min + 1) + min);
+}
+
+function startGame(){
+    setup();
+    run();
+    timer();
+}
+
+startGame();
